e225e3b23fc7f9431a0438e5fd789a91fff95a6d
Gunner Flory <g0364875@amarillocollege.com>
2025-03-17 14:02:43 -0500

    first commit

--- java_project1/Main.java
+++ java_project1/Main.java
@@ -5,6 +5,7 @@ public class Main {
     {
         Scanner scanner = new Scanner(System.in);
 
+        /* 
         //Similar to Console.WriteLine
         System.out.println("Enter Username");
 
@@ -12,11 +13,36 @@ public class Main {
 
         System.out.println("Username is: " + username);
 
+        System.out.println("Please enter the year you were born.");
+
         int year = scanner.nextInt();
 
         int age = 2025-year;
 
         System.out.println("You are " + age + " years old.");
+        */
+        Scanner myNum = new Scanner(System.in);
+        System.out.println("Please enter the first number: ");
+        int num1 = myNum.nextInt();
+        System.out.println("Please enter the second number: ");
+        int num2 = myNum.nextInt();
+        System.out.println("Please enter the third number: ");
+        int num3 = myNum.nextInt();
+
+        int largest = 0;
+
+        
+        if (num1>=num2 && num1>num3){
+            largest=num1;
+        }
+        if (num2>=num1 && num2>num3){
+            largest=num1;
+        }
+        if (num3>=num1 && num3>num2){
+            largest=num3;
+        }
+        
+        System.out.println("The largest number will be: " + largest);
 
 
 
